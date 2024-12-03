0acb9e005736f802647777b1f042087b057c3c4e
JesusDiaz64 <jesusdaviddiazcabrales@gmail.com>
2024-12-02 19:22:24 -0800

    first commit

---
+++ ejemplo0/SINTAXIS, VARIABLES, TIPSO DE DATOS Y HOLA MUNDO.py
@@ -0,0 +1,46 @@
+"""
+ * ¿Preparad@ para aprender o repasar el lenguaje de programación que tú quieras?
+ * - Recuerda que todas las instrucciones de participación están en el
+ *   repositorio de GitHub.
+ *
+ * Lo primero... ¿Ya has elegido un lenguaje?
+ * - No todos son iguales, pero sus fundamentos suelen ser comunes.
+ * - Este primer reto te servirá para familiarizarte con la forma de participar
+ *   enviando tus propias soluciones.
+ *
+ * EJERCICIO:
+ * - Crea un comentario en el código y coloca la URL del sitio web oficial del
+ *   lenguaje de programación que has seleccionado.
+ * - Representa las diferentes sintaxis que existen de crear comentarios
+ *   en el lenguaje (en una línea, varias...).
+ * - Crea una variable (y una constante si el lenguaje lo soporta).
+ * - Crea variables representando todos los tipos de datos primitivos
+ *   del lenguaje (cadenas de texto, enteros, booleanos...).
+ * - Imprime por terminal el texto: "¡Hola, [y el nombre de tu lenguaje]!"
+ *
+ * ¿Fácil? No te preocupes, recuerda que esta es una ruta de estudio y
+ * debemos comenzar por el principio.
+ 
+"""
+
+# URL: https://www.python.org/
+
+constante = ""
+
+uno = "uno"
+tres = "tres"
+
+entero = 1
+decimal = 1.2
+palabra = "Hola Mundo"
+cadena = "Hola" + "Mundo"
+booleadno = True
+lista = [1, 2, 3, "Hola Mundo"] # Dentro de una Array o lista puede tener string
+tupla = (uno, 2, tres)
+diccionario = {
+    " valor1" : 1,
+    "valor2" : 2,
+    "valor3" : 3
+}
+
+print("Hola, Python")
