9f123ee0c1f53e9455690cb9ca7a8bf43e69b91a
JesusDiaz64 <jesusdaviddiazcabrales@gmail.com>
2024-12-10 14:35:55 -0800

    Ejemplo0 ACTUALIZADO

--- POO/Unidad1/ejemplo.py
+++ POO/Unidad1/ejemplo.py
@@ -1,7 +1,9 @@
 
 
 def numero_Par_Impar():
-    num = int(input("Coloque un numero entero: "))
+    
+    print("Número par o impar sin retornar...")
+    num = int(input("Coloque un número entero: "))
     
     if num % 2 and num > 0: 
         print(f"El número {num} es impar y positivo")
@@ -19,3 +21,23 @@ def numero_Par_Impar():
         print("El numero que introdujo es nulo!")
     
 numero_Par_Impar()
+
+def retorno_Numero_Par_Impar(num):
+    
+    print("Número par o impar retornar...")
+    if num % 2 and num > 0: 
+        print(f"El número {num} es impar y positivo")
+        
+    elif num % 2 and num < 0:
+        print(f"El número {num} es impar y negativo") 
+        
+    elif num % 2 == 0 and num > 0:
+        print(f"El número {num} es par y positivo")
+        
+    elif num % 2 == 0 and num < 0:
+        print(f"El número {num} es par y negativo")
+        
+    else:
+        print("El numero que introdujo es nulo!")
+    
+retorno_Numero_Par_Impar(int(input("Coloque un otro número entero: ")))
